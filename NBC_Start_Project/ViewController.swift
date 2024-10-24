a8d7d7bdbc11015a57707169bbb9e76a2957600c
SeongGyu <junsung3807@naver.com>
2024-10-25 01:30:41 +0900

    FIRST COMMIT

---
+++ NBC_Start_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBC_Start_Project
+//
+//  Created by 전성규 on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
